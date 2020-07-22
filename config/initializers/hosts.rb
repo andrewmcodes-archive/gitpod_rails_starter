cfe0e750ff17f92f4d68e305520594efc1aae34a
Andrew Mason <andrewmcodes@protonmail.com>
2020-07-21 23:54:10 -0400

    allow any host in development

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,7 +1,10 @@
-return unless Rails.env.development?
+return unless Rails.env.development? || Rails.env.test?
 
-if ENV["NGROK_HOST"].present?
-  Rails.application.config.hosts << ENV["NGROK_HOST"]
-elsif ENV["NGROK_SUBDOMAIN"].present?
-  Rails.application.config.hosts << "#{ENV["NGROK_SUBDOMAIN"]}.ngrok.io"
-end
+# if ENV["NGROK_HOST"].present?
+#   Rails.application.config.hosts << ENV["NGROK_HOST"]
+# elsif ENV["NGROK_SUBDOMAIN"].present?
+#   Rails.application.config.hosts << "#{ENV["NGROK_SUBDOMAIN"]}.ngrok.io"
+# end
+
+# Allow accessing localhost on any domain.
+config.hosts = nil
